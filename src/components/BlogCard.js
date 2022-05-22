3a78d10fba8a9ab72ccec7a65a944014184192de
Joy <hoyyun1@gmail.com>
2022-05-22 21:30:12 +0530

    Ui change

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -6,37 +6,24 @@ const BlogCard = ({ blogData }) => {
     <>
       {blogData.map((curElem) => {
         return (
-          <>
-            <div className="col-md-6 col-lg-4">
-              <div
-                className="card my-2 "
-                style={{
-                  boxShadow:
-                    "0px 14.7px 9.5px rgba(0, 0, 0, 0.055), 0px 117px 76px rgba(0, 0, 0, 0.11)",
-                  fontFamily: "'karla', sans-serif",
-                }}
-                key={curElem.id}
-              >
-                <img
-                  src={curElem.image}
-                  class="card-img-top"
-                  alt="Programming"
-                />
-                <div className="card-body">
-                  <h2
-                    className="card-title "
-                    style={{
-                      fontFamily: "'Montserrat', sans-serif",
-                      fontWeight: "500",
-                    }}
-                  >
-                    {curElem.day}
-                  </h2>
-                  <p className="card-text">{curElem.description}</p>
-                </div>
+          <div className="col-md-6 col-lg-4" 
+          >
+            <div key={curElem.id}>
+              <div className="card-body border-primary">
+                <h2
+                  className="card-title"
+                  style={{
+                    fontFamily: "'Montserrat', sans-serif",
+                    fontWeight: "500",
+                  }}
+                >
+                  {curElem.day}
+                </h2>
+                <p className="card-text">{curElem.description}</p>
               </div>
             </div>
-          </>
+          </div>
+          
         );
       })}
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg fixed-top navbar-dark" style={{ backgroundColor: "#146e00" }}>
+      <nav className="navbar navbar-expand-lg fixed-top " style={{background: "hsl(219, 100%, 97%)" }}>
         <div className="container-fluid">
           <a className="navbar-brand" style={{ fontWeight: "700", fontFamily: "'Montserrat', sans-serif" }} href="/">
-            Joy Baruah
+            JOY BARUAH
           </a>
         </div>
       </nav>
