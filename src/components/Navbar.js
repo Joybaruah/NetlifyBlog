5675b444895a781f0ab2410600dea6554a3d3d1c
Joy <hoyyun1@gmail.com>
2022-05-22 21:48:25 +0530

    Nav bg Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg fixed-top " style={{background: "hsl(219, 100%, 97%)" }}>
+      <nav className="navbar navbar-expand-lg fixed-top bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" style={{ fontWeight: "700", fontFamily: "'Montserrat', sans-serif" }} href="/">
             JOY BARUAH
